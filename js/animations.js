09b1a1e9d08bd94e402e00560fb216063eb95373
Steven Roper <steven.m.roper@gmail.com>
2015-03-25 20:16:54 -0600

    Adding animation to textarea.

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-
 	
+	var textareaHeight = 0;
+	$('.contact-message').focus(function() {
+		textareaHeight = $('.contact-message').height();
+		console.log(textareaHeight);
+		$('.contact-message').animate({rows: 7}, 250);
+	}).blur(function() {
+		$('.contact-message').animate({rows: 1}, 250);
+	});
+
 });
