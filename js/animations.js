5063600b70ae7a207073a89de31ee882eaa33f04
Steven Roper <steven.m.roper@gmail.com>
2018-02-02 23:38:29 -0700

    Changes personal website to new React resume.

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	
-	$('.contact-message').focus(function() {
-		$('.contact-message').animate({rows: 7}, 250);
-	}).blur(function() {
-		$('.contact-message').animate({rows: 1}, 250);
-	});
-
-});
